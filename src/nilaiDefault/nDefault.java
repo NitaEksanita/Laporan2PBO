13b033d1c36359de41c9c90cd4a94dbeb4b97072
unknown <eksanitanita@gmail.com>
2015-05-02 13:48:45 +0700

    versi 1.0 Laporan 2 PBO

---
+++ src/nilaiDefault/nDefault.java
@@ -0,0 +1,35 @@
+package nilaiDefault;
+
+public class nDefault {
+	static boolean bool;
+	static byte by;
+	static float f;
+	static String \u0073\u0074\u0072 = "Praktikum nilai default dan Unicode Nita";
+	
+	public static void main(String[] args){
+		if (str == null) {		
+			System.out.println("nilai default boolean " + bool);	
+			System.out.println("nilai default Byte " + by);
+			System.out.println("nilai default Float " + f);
+			System.out.println(str);
+			
+		}
+		else {
+			Boolean refBoolean = new Boolean(true);
+			boolean bool = refBoolean.booleanValue();		
+			System.out.println("bool " + bool);
+			
+			Byte refByte = new Byte((byte) 123);
+			byte b = refByte.byteValue();		
+			System.out.println("Byte " + b);
+			
+			Float refFloat = new Float(12.3F);
+			float f = refFloat.floatValue();
+			System.out.println("Float " + f);
+			
+			System.out.println(str);
+			
+		}
+		
+	}
+}
